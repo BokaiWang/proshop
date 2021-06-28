55b8fd303e9ea19168cff797da3fa8c0317febaf
BokaiWang <bk.wang1989@gmail.com>
2021-06-28 16:22:00 -0400

    Connect Redux to HomeScreen by using useDispatch and useSelector

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,31 +1,39 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Col, Row } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
 import Product from "../components/Product";
-import axios from "axios";
+import { listProducts } from "../actions/productActions";
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+
+  const productList = useSelector((state) => {
+    return state.productList;
+  });
+  const { loading, error, products } = productList;
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get("/api/products");
-      setProducts(data);
-    };
-    fetchProducts();
-  }, []);
+    dispatch(listProducts());
+  }, [dispatch]);
 
   return (
     <>
       <h1>Latest Products</h1>
-      <Row>
-        {products.map((p) => {
-          return (
-            <Col key={p._id} sm={12} md={6} lg={4} xl={3}>
-              <Product product={p} />
-            </Col>
-          );
-        })}
-      </Row>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products.map((p) => {
+            return (
+              <Col key={p._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={p} />
+              </Col>
+            );
+          })}
+        </Row>
+      )}
     </>
   );
 };
