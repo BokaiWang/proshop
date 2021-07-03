ec05e0371c79aa9ee0e457912af930d2d76baa92
BokaiWang <bk.wang1989@gmail.com>
2021-07-03 00:09:42 -0400

    Create Authentication Middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -23,3 +23,23 @@ export const authUser = asyncHandler(async (req, res) => {
     throw new Error("Invalid email or password");
   }
 });
+
+// @desc   Get user profile
+// @route  GET /api/users/profile
+// @access Private
+export const getUserProfile = asyncHandler(async (req, res) => {
+  // REMARK: in the protect middleware we eventually save user data in req.user
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(400);
+    throw new Error("User not found");
+  }
+});
