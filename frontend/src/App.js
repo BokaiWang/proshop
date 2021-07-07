23306d32fff26a820fbc35d209fcd6484a2c1adb
BokaiWang <bk.wang1989@gmail.com>
2021-07-06 21:00:56 -0400

    Make username shown in NavBar Create Logout function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 const App = () => {
   return (
@@ -13,8 +14,9 @@ const App = () => {
       <main className="py-3">
         <Container>
           <Route exact path="/" component={HomeScreen} />
-          <Route path="/product/:id" component={ProductScreen}></Route>
-          <Route path="/cart/:id?" component={CartScreen}></Route>
+          <Route path="/product/:id" component={ProductScreen} />
+          <Route path="/cart/:id?" component={CartScreen} />
+          <Route path="/login" component={LoginScreen} />
         </Container>
       </main>
       <Footer />
