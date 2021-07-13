bef40c4c72ef4fbba14a3d2a7c43729698e5e4de
BokaiWang <bk.wang1989@gmail.com>
2021-07-13 15:24:26 -0400

    Create isAdmin middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -103,3 +103,11 @@ export const registerUser = asyncHandler(async (req, res) => {
     throw new Error("User not found!");
   }
 });
+
+// @desc   Get all profile
+// @route  GET /api/users
+// @access Private/Admin
+export const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({});
+  res.json(users);
+});
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,14 +2,15 @@ import express from "express";
 import {
   authUser,
   getUserProfile,
+  getUsers,
   registerUser,
   updateUserProfile,
 } from "../controllers/userController.js";
-import { protect } from "../middleware/authMiddleware.js";
+import { protect, isAdmin } from "../middleware/authMiddleware.js";
 
 const userRouter = express.Router();
 
-userRouter.route("/").post(registerUser);
+userRouter.route("/").post(registerUser).get(protect, isAdmin, getUsers);
 userRouter.post("/login", authUser);
 userRouter
   .route("/profile")
