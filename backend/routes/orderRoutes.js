8bd17ba133674bfbfd4deede769662960dfc47bc
BokaiWang <bk.wang1989@gmail.com>
2021-07-13 11:29:34 -0400

    Show orders on profile, and Reset user details and orders when logging out

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   addOrderItems,
+  getMyOrders,
   getOrderById,
   updateOrderToPaid,
 } from "../controllers/orderController.js";
@@ -9,6 +10,7 @@ import { protect } from "../middleware/authMiddleware.js";
 const orderRouter = express.Router();
 
 orderRouter.route("/").post(protect, addOrderItems);
+orderRouter.route("/myorders").get(protect, getMyOrders);
 orderRouter.route("/:id").get(protect, getOrderById);
 orderRouter.route("/:id/pay").put(protect, updateOrderToPaid);
 
