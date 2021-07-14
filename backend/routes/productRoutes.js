6cf37a1b639cda4ce4ae3946d329d91d033c3865
BokaiWang <bk.wang1989@gmail.com>
2021-07-14 14:50:33 -0400

    Create deleteProduct feature

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,11 +2,16 @@ import express from "express";
 import {
   getProducts,
   getProductById,
+  deleteProduct,
 } from "../controllers/productController.js";
+import { protect, isAdmin } from "../middleware/authMiddleware.js";
 
 const productRouter = express.Router();
 
 productRouter.route("/").get(getProducts);
-productRouter.route("/:id").get(getProductById);
+productRouter
+  .route("/:id")
+  .get(getProductById)
+  .delete(protect, isAdmin, deleteProduct);
 
 export default productRouter;
