f3fb59e1bf1de9a99a4758ed7b462fa3df6a1d3d
BokaiWang <bk.wang1989@gmail.com>
2021-07-15 22:27:38 -0400

    Create Amin Order List

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -3,13 +3,17 @@ import {
   addOrderItems,
   getMyOrders,
   getOrderById,
+  getOrders,
   updateOrderToPaid,
 } from "../controllers/orderController.js";
-import { protect } from "../middleware/authMiddleware.js";
+import { isAdmin, protect } from "../middleware/authMiddleware.js";
 
 const orderRouter = express.Router();
 
-orderRouter.route("/").post(protect, addOrderItems);
+orderRouter
+  .route("/")
+  .post(protect, addOrderItems)
+  .get(protect, isAdmin, getOrders);
 orderRouter.route("/myorders").get(protect, getMyOrders);
 orderRouter.route("/:id").get(protect, getOrderById);
 orderRouter.route("/:id/pay").put(protect, updateOrderToPaid);
